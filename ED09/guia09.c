f3ff36031dd20b42c4b241c8d28e860d313aeef9
Felipe Costa Unsonst <161458493+felipeunsonst@users.noreply.github.com>
2025-02-20 15:39:49 -0300

    Update guia09.c

--- ED09/guia09.c
+++ ED09/guia09.c
@@ -36,51 +36,343 @@ void metodo0911(){
 }
 
 void matrizNoArquivo(int l, int c, int m[l][c], FILE *ma){
-
+	for(int i = 0; i < l; i++){
+		for(int j = 0; j < c; j++){
+			fprintf(ma, "%d ", m[i][j]);  
+		}
+		fprintf(ma, "\n");
+	}
 }
 
 void metodo0912(){
+	int linha, coluna;
+	printf("Digite a quantidade de linhas e colunas da matriz:\n");
+	scanf("%d", &linha);
+	scanf("%d", &coluna);
 
+	int matriz[linha][coluna];
+
+	printf("Agora digite os termos da matriz:\n");
+
+	for(int i = 0; i < linha; i++){
+		for(int j = 0; j < coluna; j++){
+			scanf("%d", &matriz[i][j]);
+		}
+	}
+
+	FILE *arq;
+	arq = fopen("MATRIZ_01.txt", "w");
+
+	if(arq == NULL){
+		printf("Falha ao abrir o arquivo.\n");
+		return;  
+	}
+
+	matrizNoArquivo(linha, coluna, matriz, arq);
+
+	fclose(arq);  
+	printf("matriz gravada no arquivo com sucesso.\n");
 }
 
-void metodo0913(){
 
+void metodo0913(){
+	int linha, coluna;
+	printf("digite a quantidade de linhas e colunas:\n");
+	scanf("%d %d", &linha, &coluna);
+
+	if(linha = coluna){
+		int matriz[linha][coluna];
+		printf("digite os valores da matriz:\n");
+		for(int i = 0; i < linha; i++){
+			for(int j = 0; j < coluna; j++){
+				scanf("%d", &matriz[i][j]);
+			}
+		}
+
+		for(int i=0; i<linha; i++){
+			for(int j=0; j<coluna; j++){
+				if(i = j){
+					printf("%d ", matriz[i][j]);
+				}
+			}
+			printf("\n");
+		}
+	} else{
+		printf("a matriz nao e quadrada, portanto, não sera impresso sua diagonal");
+	}
 }
 
 void metodo0914(){
+	int linha, coluna;
+	printf("digite a quantidade de linhas e colunas:\n");
+	scanf("%d %d", &linha, &coluna);
 
+	if(linha = coluna){
+		int n = linha;
+		int matriz[n][n];
+		printf("digite os valores da matriz:\n");
+		for(int i = 0; i < n; i++){
+			for(int j = 0; j < n; j++){
+				scanf("%d", &matriz[i][j]);
+			}
+		}
+
+		for(int i=0; i<n; i++){
+			for(int j=0; j<n; j++){
+				if(j+i == n-1){
+					printf("%d ", matriz[i][j]);
+				}
+			}
+		}
+			printf("\n");
+		} else{
+		printf("a matriz nao e quadrada, portanto, não sera impresso sua diagonal");
+	}
 }
 
-void metodo0915(){
 
+void metodo0915() {
+    int linha, coluna;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
+
+    int matriz[linha][coluna];
+
+   printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
+
+     if (linha == coluna) {  
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
+				if(j<=i){
+					 printf("%d ", matriz[i][j]); 
+				}
+            }
+            printf("\n");
+        }
+    } else {
+        printf("a matriz não é quadrada.\n");
+    }
 }
 
-void metodo0916(){
 
+void metodo0916() {
+    int linha, coluna;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
+
+    int matriz[linha][coluna];
+
+    printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            } 
+        }
+
+	if (linha == coluna) {  
+        for (int i = 0; i < linha; i++) {
+			for (int k = 0; k < i; k++) {
+                printf("  "); 
+            }
+            for (int j = i; j < linha; j++) {
+				if(j>=i){
+					printf("%d ", matriz[i][j]); 
+				} 
+            }
+            printf("\n");
+        }
+    } else {
+       printf("a matriz não é quadrada.\n");
+    }
+   
 }
 
-void metodo0917(){
+void metodo0917() {
+    int linha, coluna;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
 
+    int matriz[linha][coluna];
+
+   printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            } 
+        }
+
+    if (linha == coluna) {  
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < linha - i - 1; j++) {
+                printf("  ");  
+            }
+            for (int j = linha - i - 1; j < linha; j++) {
+                printf("%.d ", matriz[i][j]);
+            }
+            printf("\n");
+        }
+    } else {
+       printf("a matriz não é quadrada.\n");
+    } 
 }
+
 
 void metodo0918(){
+    int linha, coluna;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
+
+    int matriz[linha][coluna];
+
+   printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    if (linha == coluna) {
+        for (int i = 0; i < linha; i++) {
+            for (int j = 0; j < coluna; j++) {
+                if (j < (linha - 1 - i)) {
+                    printf("%d ", matriz[i][j]); 
+                } else if (j == (linha - 1 - i)) {
+                    printf("%d ", matriz[i][j]); 
+                } else {
+                    printf("  "); 
+                }
+            }
+            printf("\n");
+        }
+    } else {
+        printf("a matriz não é quadrada.\n");
+    }
 
 }
 
 void metodo0919(){
+	int linha, coluna;
+	int zero = 0;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
 
+    int matriz[linha][coluna];
+
+   printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            }
+        }
+
+     if (linha == coluna) {  
+       for (int i = 0; i < linha; i++) {
+        for (int j = 0; j <= i; j++) { 
+            if (matriz[i][j] != 0) {
+                zero++; 
+            }
+        }
+    }
+
+    if (zero > 0) {
+        printf("nao sao todos os valores que sao zeros.\n");
+    } else {
+        printf("todos os valores sao zeros.\n");
+    }
+    } else {
+        printf("a matriz não é quadrada.\n");
+    }
 }
 
 void metodo0920(){
+	 int linha, coluna;
+	int zero=0;
+    printf("digite a quantidade de linhas e colunas:\n");
+    scanf("%d %d", &linha, &coluna);
 
+    int matriz[linha][coluna];
+
+    printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            } 
+        }
+
+	if (linha == coluna) {  
+       for (int i = 0; i < linha; i++) {
+        for (int j = i + 1; j < coluna; j++) { 
+            if (matriz[i][j] != 0) {
+                zero++; 
+            }
+        }
+    }
+
+  
+	    if (zero > 0) {
+	        printf("nao sao todos os valores que sao zeros acima da diagonal principal.\n");
+	    } else {
+	        printf("todos os valores acima da diagonal principal sao zeros.\n");
+	    }
+    } else {
+       printf("a matriz nao e quadrada.\n");
+    }
+   
 }
 
 void metodo09E1(){
+	int linha, coluna;
+	printf("digite a quantidade de linhas e colunas:\n");
+	scanf("%d", &linha);
+	getchar();
+	scanf("%d", &coluna);
+	getchar();
 
+	int matriz[linha][coluna];
+
+	 printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            } 
+        }
+
+	for (int j = 0; j < coluna; j++) {
+        for (int i = 0; i < linha; i++) {
+                printf("%d ", matriz[i][j]); 
+            } 
+		printf("\n");
+        }
 }
 
 void metodo09E2(){
+	int linha, coluna;
+	printf("digite a quantidade de linhas e colunas:\n");
+	scanf("%d", &linha);
+	getchar();
+	scanf("%d", &coluna);
+	getchar();
 
+	int matriz[linha][coluna];
+
+	 printf("digite os valores da matriz:\n");
+    for (int i = 0; i < linha; i++) {
+        for (int j = 0; j < coluna; j++) {
+                scanf("%d", &matriz[i][j]);
+            } 
+        }
+
+	for (int i = linha-1; i >= 0; i--) {
+        for (int j = coluna-1; j >=0; j--) {
+                printf("%d ", matriz[i][j]);
+            } 
+		printf("\n");
+        }
 }
 
 int main(){
@@ -88,7 +380,7 @@ int opcao;
 
 
     do{
-    printf("digite o numero correspondente ao metodo desejado\n0-encerrar programa\n1-metodo099911\n2-metodo099912\n3-metodo099913\n4-metodo099914\n5-metodo099915\n6-metodo099916\n7-metodo099917\n8-metodo099918\n9-metodo099919\n10-metodo099920\n11-metodo0999E1\n12-metodo0999E2\n");
+    printf("digite o numero correspondente ao metodo desejado\n0-encerrar programa\n1-metodo0911\n2-metodo0912\n3-metodo0913\n4-metodo0914\n5-metodo0915\n6-metodo0916\n7-metodo0917\n8-metodo0918\n9-metodo0919\n10-metodo0920\n11-metodo09E1\n12-metodo0999E2\n");
     scanf("%d", &opcao);
 	getchar();
 
